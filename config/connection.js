9f4c1cbb3c3793cad12ffa7c1235eadb55c66aee
Brian Autieri <119701025+Brian-Autieri@users.noreply.github.com>
2023-06-28 10:01:04 -0700

    Upload connection.js

---
+++ config/connection.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+// Connects to Mongoose database and exports the connection for use in other parts of the application
+mongoose.connect(
+    process.env.MONGODB_URI || "mongodb://localhost:27017/social-network-api",
+    {
+        useNewUrlParse: true,
+        useUnifiedTopology: true,
+    }
+);
+
+mongoose.set("debug", true);
+
+module.exports = mongoose.connection;
